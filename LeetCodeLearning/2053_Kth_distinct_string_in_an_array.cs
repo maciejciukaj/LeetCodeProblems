9443da20a473f02cce235932f39388ed16fa46b2
maciej_ciukaj <maciek.ciukaj@gmail.com>
2024-08-05 18:26:48 +0200

    Solution for problem 2053 added

---
+++ LeetCodeLearning/2053_Kth_distinct_string_in_an_array.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCodeLearning
+{
+    internal class _2053_Kth_distinct_string_in_an_array
+    {
+        public static string KthDistinct(string[] arr, int k)
+        {
+            Dictionary<string, int> keyValuePairs = new Dictionary<string, int>(); ;
+            foreach (var elem in arr)
+            {
+                if (keyValuePairs.ContainsKey(elem))
+                {
+                    keyValuePairs[elem]++;
+                }
+                else
+                {
+                    keyValuePairs[elem] = 1;
+                }
+            }
+
+            foreach (var pair in keyValuePairs)
+            {
+                if (pair.Value == 1)
+                {
+                    k--;
+                    if (k == 0)
+                    {
+                        return pair.Key;
+                    }
+                }
+            }
+            return "";
+        }
+    }
+}
