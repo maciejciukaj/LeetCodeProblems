85889c66ed573ec7ea3566dfde7700b510fa6f59
maciej_ciukaj <maciek.ciukaj@gmail.com>
2024-08-15 13:16:43 +0200

    Added solution for problem no. 860

---
+++ LeetCodeLearning/860_Lemonade_change.cs
@@ -0,0 +1,41 @@
+﻿//https://leetcode.com/problems/lemonade-change/description/
+
+namespace LeetCodeLearning
+{
+    public class _860_Lemonade_change
+    {
+        public static bool LemonadeChange(int[] bills)
+        {
+            int five = 0, ten = 0;
+            foreach (var bill in bills)
+            {
+                if (bill == 5)
+                    five++;
+
+                else if (bill == 10)
+                {
+                    if (five >= 1)
+                    {
+                        five--;
+                        ten++;
+                    }
+                    else
+                        return false;
+                }
+                else
+                {
+                    if (five >= 1 && ten >= 1)
+                    {
+                        ten--;
+                        five--;
+                    }
+                    else if (five >= 3)
+                        five -= 3;
+                    else
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
